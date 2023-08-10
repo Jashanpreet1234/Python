e017b50573d7eb9aa36d740fa73a351300734f2b
jashanpreet1234 <jashanpreetkaur2904@gmail.com>
2023-08-10 15:26:53 -0400

    Function

--- basics.py
+++ basics.py
@@ -12,3 +12,32 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+#CASTING
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+# Multiple variables
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+#Function
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
