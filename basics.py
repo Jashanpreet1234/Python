ef89ba03fb6ee97ffa2d24123e56b5153d12c739
jashanpreet1234 <jashanpreetkaur2904@gmail.com>
2023-08-10 15:48:01 -0400

    Function

--- basics.py
+++ basics.py
@@ -1,22 +1,9 @@
-""" CREATING A FUNCTION IN PYTHON
-We can create a user-defined
-function in Python, using the def keyword. We can add any type of functionalities and properties to it as we require.
-"""
-def fun():
-print("Welcome to GFG")
+x = "awesome"
 
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
 
-""" After creating a function in Python we can call
- it by using the name of the function followed by parenthesis 
- containing parameters of that particular function."""
+myfunc()
 
-# A simple Python function
-def fun():
-    print("Welcome to GFG")
-
-
-# Driver code to call a function
-fun()
-
-#Defining and calling a function with parameters
-
+print("Python is " + x)
