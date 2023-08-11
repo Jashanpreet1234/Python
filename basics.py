e59a7099d77249ff3dce8ed04f98bc589ac0d499
jashanpreet1234 <jashanpreetkaur2904@gmail.com>
2023-08-11 10:48:57 -0400

    OPerators

--- basics.py
+++ basics.py
@@ -1,45 +1,14 @@
-#Join all items in a tuple into a string, using a hash character as separator:
-myTuple = ("John", "Peter", "Vicky")
+#Exponentiation
+x = 3
+y = 5
 
-x = "#".join(myTuple)
+print(x ** y) #same as 3*3*3*3*3
 
-print(x)
+#floor division
 
-#Boolean
+x = 15
+y = 2
 
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+print(x // y) # 15/2 = 7
 
-
-a = 200
-b = 33
-
-if b > a:
-  print("b is greater than a")
-else:
-  print("b is not greater than a")
-
-
-print(bool("Hello"))
-print(bool(15))
-
-print(bool("abc"))
-print(bool(123))
-print(bool(["apple", "cherry", "banana"]))
-
-
-#function can return a boolean
-print("hi")
-def myFunction() :
-  return True
-
-print(myFunction())
-
-def myFunction() :
-  return True
-
-if myFunction():
-  print("YES!")
-else:
-  print("NO!")
+#the floor division // rounds the result down to the nearest whole number
